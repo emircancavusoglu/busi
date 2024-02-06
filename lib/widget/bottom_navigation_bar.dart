2182b74609a9e833f413b7970d19c2dc008409b5
emircan_cavusoglu_16@hotmail.com <emircan_cavusoglu_16@hotmail.com>
2024-02-07 01:32:16 +0300

    add onTapped function

--- lib/widget/bottom_navigation_bar.dart
+++ lib/widget/bottom_navigation_bar.dart
@@ -8,16 +8,25 @@ class BottomNavigation extends StatefulWidget {
 }
 
 class _BottomNavigationState extends State<BottomNavigation> {
-  
+
+  var _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: _selectedIndex,
       type: BottomNavigationBarType.fixed,
         items: const <BottomNavigationBarItem>[
-      BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Ana Sayfa'),
+      BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Ana Sayfa',),
       BottomNavigationBarItem(icon: Icon(Icons.analytics_outlined),label: 'Analiz Yap'),
       BottomNavigationBarItem(icon: Icon(Icons.format_list_numbered),label: 'Sektör '),
       BottomNavigationBarItem(icon: Icon(Icons.settings),label: 'Ayarlar'),
-        ]);
+        ],
+    onTap: _onTapped
+    );
   }
+  void _onTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+     }
 }
